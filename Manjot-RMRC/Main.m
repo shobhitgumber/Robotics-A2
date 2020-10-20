3f0b7bd234e6aec49a79b71092a3451838a1746c
Jot95 <41608464+Jot95@users.noreply.github.com>
2020-10-21 04:51:00 +1100

    orientation and locations fixed, made switch case
    - fixed orientation and locations of pizza pan and sauce bottles
    -made a switch case for sauce order depending on what sauce the customer wants 
    - need to clean up variables still

--- Manjot-RMRC/Main.m
+++ Manjot-RMRC/Main.m
@@ -1,91 +1,110 @@
-EnvironmentGen();
-
-Brick1Tr = transl(1, 1.4, 1)*troty(-pi);
-BrickTr = transl(1, 1.7, 1)*troty(-pi);
+%EnvironmentGen();
+robot1base = [0.2, 1.5, 0.83];
+Brick1Tr = transl(robot1base(1)+0.8, robot1base(2) -0.1, 1)*troty(-pi);
+BrickTr = transl(robot1base(1)+0.8, 1.7, 1)*troty(-pi);
 Brick2Tr = transl(0, 0.5, 0.87);
 
 qkino = zeros(1, 6); 
-robot1base = [0.2, 1.5, 0.83]; %Base of the robot offset 
 robot = kino('kino', robot1base); %Initializing Robot 1
 hold on;
-robot.model.base = transl(robot1base) * trotz(-pi);
-robot.model.animate(qkino) %Animate UR3
+robot.model.base = transl(robot1base)*trotz(pi);
+robot.model.animate(qkino); %Animate UR3
 
 Brick_1 = ImportPly('IngBottle.ply', Brick1Tr);
 Brick = ImportPly('IngBottle2.ply', BrickTr);
 Brick_2 = ImportPly('PBase.ply', Brick2Tr);
 
-disp('PRESS ANY KEY TO ADD SAUCE 1 - PHASE 1...');
-pause;
-
-%% PHASE 1 MOVING FIRST SAUCE BOTTLE To Pizza
-camlight;
-
-UR5Movement1 = transl(Brick1Tr(1,4), Brick1Tr(2,4), 0.95);
-UR3Movement2Tr = Brick1Tr*trotx(pi)*trotz(-pi);
-UR5Movement = [UR5Movement1,UR3Movement2Tr];
-kino.Move(robot, UR5Movement, [] );
 
 
-T2 = transl(0,0.5,1.1);
-T3 = transl(0,0.5,1.1)*trotx(-3*pi/2);
-UR5Movement = [T2,T3];
-kino.Move(robot, UR5Movement, Brick_1 );
+n = input("FOR BBQ BASE ENTER 1 // FOR TOMATO BASE ENTER 2: ")
+
+switch n 
+    case 1 
+        disp( "YOU HAVE SELECTED BBQ BASE");
+        disp( "APPLYING SAUCE NOW");
+
+        %% PHASE 1 MOVING FIRST SAUCE BOTTLE To Pizza
+        camlight;
+
+        UR5Movement1 = transl(Brick1Tr(1,4), Brick1Tr(2,4), 0.95)*trotx(-pi)*trotz(pi);
+        UR3Movement2Tr = Brick1Tr*trotx(pi)*trotz(-pi);
+        UR5Movement = [UR5Movement1,UR3Movement2Tr];
+        kino.Move(robot, UR5Movement, [] );
 
 
-disp('PRESS ANY KEY TO RESET ROBOT ARM - PHASE 2...');
-pause;
+        T2 = transl(0,0.5,1.1);
+        T3 = transl(0,0.5,1.1)*trotx(-3*pi/2);
+        UR5Movement = [T2,T3];
+        kino.Move(robot, UR5Movement, Brick_1 );
 
-%% PHASE 2 - RESET 
+        disp('SAUCE APPLIED...')
+        disp('PRESS ANY KEY TO RESET ROBOT ARM - PHASE 2...');
+        pause;
+
+        %% PHASE 2 - RESET 
  
-T4= transl(1, 1.4,0.25)
-UR5Movement = [T3,T2,UR5Movement1, T4];
-kino.Move(robot, UR5Movement, Brick_1 );
+        T4= transl(0.5, 0.5,0.25);
+        T41= transl(1, 1.4,0.10);
 
-T5= transl(0.2, 1.4,1)
+        UR5Movement = [T2,T4,T41];
+        kino.Move(robot, UR5Movement, Brick_1 );
 
-UR6Movement = [T5];
+        T5= transl(0.2, 1.4,0.95)
 
-kino.Move(robot, UR6Movement, [] );
-robot.model.animate(qkino); %Animate UR3
+        UR6Movement = [T5];
 
-disp('PRESS ANY KEY TO ADD SECOND SAUCE - PHASE 3...');
-pause;
+        kino.Move(robot, UR6Movement, [] );
+       
+        
+       
+    
+    case 2 
+       camlight;
+       disp( "YOU HAVE SELECTED TOMATO BASE");
+       disp( "APPLYING SAUCE NOW");
 
-%% PHASE 3 - BOTTLE 2 
+        %% PHASE 3 - SAUCE BOTTLE 2 
+        Movement1 = transl(BrickTr(1,4), BrickTr(2,4), 0.95)*trotx(-pi)*trotz(pi);
+        Movement2Tr = BrickTr*trotx(pi)*trotz(-pi);
+        Movement = [Movement1,Movement2Tr];
+        kino.Move(robot, Movement, [] );
 
-Movement1 = transl(BrickTr(1,4), BrickTr(2,4), 0.95);
-Movement2Tr = BrickTr*trotx(pi)*trotz(-pi);
-Movement = [Movement1,Movement2Tr];
-kino.Move(robot, Movement, [] );
-
-T6 = transl(Brick1Tr(1,4), Brick1Tr(2,4), 0.95);
-T7 = transl(0,0.5,1.1);
-T8 = transl(0,0.5,1.1)*trotx(-3*pi/2);
-UR5Movement = [T6,T7,T8];
-kino.Move(robot, UR5Movement, Brick);
+        T6 = transl(Brick1Tr(1,4), Brick1Tr(2,4), 0.95);
+        T7 = transl(0,0.5,1.1);
+        T8 = transl(0,0.5,1.1)*trotx(-3*pi/2);
+        UR5Movement = [T6,T7,T8];
+        kino.Move(robot, UR5Movement, Brick);
 
 
-disp('PRESS ANY KEY TO RESET ROBOT ARM - PHASE 4...');
-pause;
-%% PHASE 4 - RESET 
+        disp('PRESS ANY KEY TO RESET ROBOT ARM - PHASE 4...');
+        pause;
+        %% PHASE 4 - RESET 
  
-T9= transl(1, 1.7,0.25)
-UR5Movement = [T8,T7,T6, T9];
-kino.Move(robot, UR5Movement, Brick );
+        T4= transl(0.5, 0.5,0.25);
+        T41= transl(1, 1.4,0.10);
 
-T10= transl(0.2, 1.7,1)
+        UR5Movement = [T2,T4,T41];
+        kino.Move(robot, UR5Movement, Brick );
 
-UR6Movement = [T10];
+        T5= transl(0.2, 1.4,0.95)
 
-kino.Move(robot, UR6Movement, [] );
-robot.model.animate(qkino); %Animate UR3
+        UR6Movement = [T5];
 
-disp('PRESS ANY KEY TO COOK THE PIZZA - PHASE 5...');
-pause;
+        kino.Move(robot, UR6Movement, [] );
+        
+    otherwise 
+        
+        disp("PLEASE CHOOSE FROM THE OPTIONS, THANK YOU...")
+        
+        
+end
+
 
 %% PHASE 5 - COOKING PIZZA 
 
+
+disp('BASE SELECTED, COOKING PIZZA NOW')
+%robot.model.animate(qkino); %Animate UR3
 Brick2Tr= transl(0.5, 0.5,0.88);
 Brick_2 = ImportPly('PBase.ply', Brick2Tr);
 Brick2Tr = transl(1.0, 0.5,0.89);
@@ -102,6 +121,7 @@ pause;
 
 %% PHASE 6 - PIZZA COOKED, EXIT OVEN 
 
+disp('PIZZA FINISHED COOKING')
 Brick2Tr= transl(2.5, 0.5,0.92);
 Brick_2 = ImportPly('PBase.ply', Brick2Tr);
 Brick2Tr = transl(2.0, 0.5,0.91);
@@ -120,15 +140,23 @@ pause;
 
 %% PHASE 7 - PIZZA READY, PUT IT ON THE TABLE 
 
-T11 = transl(0, 0.5,0.87);
-T12 = transl(0, 0.5,0.87)*trotx(-3*pi/2);
+disp('PICKING UP PIZZA')
+T11= transl(0,0.5,0.87)*trotx(-pi)*trotz(pi);
+T12 = transl(0,0.5,0.90)*trotx(-3*pi/2);
+%T11 = transl(0, 0.5,0.87)*trotx(-pi)*trotz(pi);
+%T12 = Brick2Tr*trotx(pi)*trotz(-pi);
+%transl(0, 0.5,0.87)
+%*trotx(-3*pi/2);
 Movement7 = [T11,T12]
 kino.Move(robot, Movement7,[]);
 
 T13 = transl(-1, 1.5, 0.83);
-T14 = transl(-1, 1.5, 0.83)*trotx(-pi/2);
+T14 = transl(-1, 1.5, 1.1)*trotx(-pi/2);
 Movement8 = [T13,T14]
 kino.Move(robot,Movement8,Brick_2);
+
+
+disp('BON APPETIT');
 
 
 
