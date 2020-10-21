d75d8d9a72361039796a21eef7f33699c73662bf
shobhitgumber <68949834+shobhitgumber@users.noreply.github.com>
2020-10-21 12:41:30 +1100

    Add files via upload

---
+++ Main.m
@@ -0,0 +1,174 @@
+EnvironmentGen();
+robot1base = [0.2, 1.5, 0.83];
+BBQTr = transl(robot1base(1)+0.8, robot1base(2) -0.1, 1)*troty(-pi);
+TomatoTr = transl(robot1base(1)+0.8, 1.7, 1)*troty(-pi);
+pizzaPanTr = transl(0, 0.5, 0.87);
+
+qkino = zeros(1, 6); 
+robot = kino;
+%Initializing Robot 1
+hold on;
+robot.model.base = transl(robot1base)*trotz(pi);
+robot.model.animate(qkino); 
+
+BBQ_BOTTLE = ImportPly('IngBottle.ply', BBQTr);
+TOMATO_BOTTLE = ImportPly('IngBottle2.ply', TomatoTr);
+pizzaPan = ImportPly('PBase.ply', pizzaPanTr);
+
+
+
+n = input("FOR BBQ BASE ENTER 1 // FOR TOMATO BASE ENTER 2: ")
+
+switch n 
+    case 1 
+        disp( "YOU HAVE SELECTED BBQ BASE");
+        disp( "APPLYING SAUCE NOW");
+
+        %% PHASE 1 MOVING FIRST SAUCE BOTTLE To Pizza
+        camlight;
+
+        Sauce1Movement1 = transl(BBQTr(1,4), BBQTr(2,4), 0.95)*trotx(-pi)*trotz(pi);
+        Sauce1Movement2 = BBQTr*trotx(pi)*trotz(-pi);
+        Sauce1Movement = [Sauce1Movement1,Sauce1Movement2];
+        kino.Move(robot, Sauce1Movement, [] );
+
+
+        T2 = transl(0,0.5,1.1);
+        T3 = transl(0,0.5,1.1)*trotx(-3*pi/2);
+        Sauce1Movement = [T2,T3];
+        kino.Move(robot, Sauce1Movement, BBQ_BOTTLE );
+
+        disp('SAUCE APPLIED...')
+        disp('PRESS ANY KEY TO RESET ROBOT ARM - PHASE 2...');
+        pause;
+
+        %% PHASE 2 - RESET 
+ 
+        T4= transl(0.5, 0.5,0.25);
+        T41= transl(1, 1.4,0.10);
+
+        Sauce1Movement = [T2,T4,T41];
+        kino.Move(robot, Sauce1Movement, BBQ_BOTTLE );
+
+        T5= transl(0.2, 1.4,0.95)
+
+        RESETMovement = [T5];
+
+        kino.Move(robot, RESETMovement, [] );
+       
+        
+       
+    
+    case 2 
+       camlight;
+       disp( "YOU HAVE SELECTED TOMATO BASE");
+       disp( "APPLYING SAUCE NOW");
+
+        %% PHASE 3 - SAUCE BOTTLE 2 
+        Movement1 = transl(TomatoTr(1,4), 1.7, 0.95)*trotx(-pi)*trotz(pi);
+        Movement2Tr = TomatoTr*trotx(pi)*trotz(-pi);
+        Movement = [Movement1,Movement2Tr];
+        kino.Move(robot, Movement, [] );
+
+        T6 = transl(BBQTr(1,4), BBQTr(2,4), 0.95);
+        T7 = transl(0,0.5,1.1);
+        T8 = transl(0,0.5,1.1)*trotx(-3*pi/2);
+        Sauce2Movement = [T6,T7,T8];
+        kino.Move(robot, Sauce2Movement, TOMATO_BOTTLE);
+
+
+        disp('PRESS ANY KEY TO RESET ROBOT ARM - PHASE 4...');
+        pause;
+        %% PHASE 4 - RESET 
+ 
+        T4= transl(0.5, 0.5,0.25);
+        T41= transl(1, 1.4,0.10);
+
+        Sauce2Movement = [T2,T4,T41];
+        kino.Move(robot, Sauce2Movement, TOMATO_BOTTLE );
+
+        T5= transl(0.2, 1.4,0.95)
+
+        RESETMovement = [T5];
+
+        kino.Move(robot, RESETMovement, [] );
+        
+    otherwise 
+        
+        disp("PLEASE CHOOSE FROM THE OPTIONS, THANK YOU...")
+        
+        
+end
+
+
+%% PHASE 5 - COOKING PIZZA 
+
+
+disp('BASE SELECTED, COOKING PIZZA NOW')
+pizzaPanTr= transl(0.5, 0.5,0.88);
+pizzaPan = ImportPly('PBase.ply', pizzaPanTr);
+pizzaPanTr = transl(1.0, 0.5,0.89);
+pizzaPan = ImportPly('PBase.ply', pizzaPanTr);
+pizzaPanTr = transl(1.5, 0.5,0.90);
+pizzaPan = ImportPly('PBase.ply', pizzaPanTr);
+pizzaPanTr = transl(2.0, 0.5,0.91);
+pizzaPan = ImportPly('PBase.ply', pizzaPanTr);
+pizzaPanTr = transl(2.5, 0.5,0.92);
+pizzaPan = ImportPly('PBase.ply', pizzaPanTr);
+
+disp('PRESS ANY KEY TO FINISH COOKING THE PIZZA - PHASE 6...');
+pause;
+
+%% PHASE 6 - PIZZA COOKED, EXIT OVEN 
+
+disp('PIZZA FINISHED COOKING')
+pizzaPanTr= transl(2.5, 0.5,0.92);
+pizzaPan = ImportPly('PBase.ply', pizzaPanTr);
+pizzaPanTr = transl(2.0, 0.5,0.91);
+pizzaPan = ImportPly('PBase.ply', pizzaPanTr);
+pizzaPanTr = transl(1.5, 0.5,0.90);
+pizzaPan = ImportPly('PBase.ply', pizzaPanTr);
+pizzaPanTr = transl(1.0, 0.5,0.89);
+pizzaPan = ImportPly('PBase.ply', pizzaPanTr);
+pizzaPanTr = transl(0.5, 0.5,0.88);
+pizzaPan = ImportPly('PBase.ply', pizzaPanTr);
+pizzaPanTr = transl(0, 0.5,0.87);
+pizzaPan = ImportPly('PBase.ply', pizzaPanTr);
+
+disp('PRESS ANY KEY FOR ROBOT TO PICK UP THE PIZZA - PHASE 7...');
+pause;
+
+%% PHASE 7 - PIZZA READY, PUT IT ON THE TABLE 
+
+disp('PICKING UP PIZZA')
+T11= transl(0,0.5,0.87)*trotx(-pi)*trotz(pi);
+T12 = transl(0,0.5,0.90)*trotx(-3*pi/2);
+%T11 = transl(0, 0.5,0.87)*trotx(-pi)*trotz(pi);
+%T12 = pizzaPanTr*trotx(pi)*trotz(-pi);
+%transl(0, 0.5,0.87)
+%*trotx(-3*pi/2);
+Movement7 = [T11,T12]
+kino.Move(robot, Movement7,[]);
+
+T13 = transl(-1, 1.5, 0.83);
+T14 = transl(-1, 1.5, 1.1)*trotx(-pi/2);
+Movement8 = [T13,T14]
+kino.Move(robot,Movement8,pizzaPan);
+
+
+disp('BON APPETIT');
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
--- kino.m
+++ kino.m
@@ -2,25 +2,18 @@ classdef kino < handle
     properties
         %> Robot model
         model;
-        steps=150;
-
+        name;
+        base_position=[0 0 0];
         %>
         workspace = [-2 2 -2 2 -0.3 3];   
         
-        %> Flag to indicate if gripper is used
-        useGripper = false;    
+        %> Flag to indicate if gripper is used  
     end
     properties(Access= public)
         gui;
     end
     methods%% Class for kino robot simulation
-function self = kino(useGripper)
-    if nargin < 1
-        useGripper = false;
-    end
-    self.useGripper = useGripper;
-    
-%> Define the boundaries of the workspace
+function self = kino
         
 % robot = 
 self.gui=app1;
@@ -39,7 +32,7 @@ function GetkinoRobot(self)
 %     if nargin < 1
         % Create a unique name (ms timestamp after 1ms pause)
         pause(0.001);
-        name = ['kino',datestr(now,'yyyymmddTHHMMSSFFF')];
+        self.name = ['kino',datestr(now,'yyyymmddTHHMMSSFFF')];
 %     end
             L1 = Link('d',0.2848,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360 360]),'offset',0);
             L2= Link('d',0.0054,'a',0.41,'alpha',-pi,'qlim',deg2rad([-128.9 128.9]),'offset',-pi/2);
@@ -50,8 +43,10 @@ function GetkinoRobot(self)
 
             
 
-    self.model = SerialLink([ L1 L2 L3 L4 L5 L6 ],'name',name);
+    self.model = SerialLink([ L1 L2 L3 L4 L5 L6 ],'name',self.name);
 %     self.model.base= transl(0,0,0)*self.model.base;
+                     self.model.base = se3(se2(self.base_position));
+
 
 
 end
@@ -62,11 +57,8 @@ end
 % colour them in if data is available 
 function PlotAndColourRobot(self)%robot,workspace)
     for linkIndex = 0:self.model.n
-        if self.useGripper && linkIndex == self.model.n
-            [ faceData, vertexData, plyData{linkIndex+1} ] = plyread(['Kino_Link',num2str(linkIndex),'Gripper.ply'],'tri'); %#ok<AGROW>
-        else
             [ faceData, vertexData, plyData{linkIndex+1} ] = plyread(['Kino_Link',num2str(linkIndex),'.ply'],'tri'); %#ok<AGROW>
-        end
+        
         self.model.faces{linkIndex+1} = faceData;
         self.model.points{linkIndex+1} = vertexData;
     end
@@ -92,7 +84,40 @@ function PlotAndColourRobot(self)%robot,workspace)
             continue;
         end
     end
-end 
-
+end
+    end
+methods (Static)
+      %TAKEN AND DERIVED FROM LAB EXCERCISES USING TRAPEZOIDAL PROFILE
+        function Move(robot, movement, component)
+            [rows, cols] = size(movement);
+            for i = 1:cols/4
+                currentPose = robot.model.getpos()';
+                nextPose = robot.model.ikcon(movement(:,(4*i - 3):4*i), currentPose);
+                
+                minimumIndex = nextPose' < robot.model.qlim(:,1);
+                nextPose(minimumIndex) = robot.model.qlim(minimumIndex,1) + 10*pi/180;
+                maximumIndex = robot.model.qlim(:,2) < nextPose';
+                nextPose(maximumIndex) = robot.model.qlim(maximumIndex,2) - 10*pi/180;
+                [tr_EndEffector, jointVariables] = robot.model.fkine(nextPose);
+                
+                steps = 50;
+                s = lspb(0,1,steps);
+                qMatrix = nan(steps,6);
+                for i = 1:steps
+                    qMatrix(i,:) = (1-s(i))*currentPose + s(i)*nextPose';
+                end
+                for movementIndex = 1:size(qMatrix,1)
+                    animate(robot.model,qMatrix(movementIndex,:));
+                    tr_EndEffector = robot.model.fkine(qMatrix(movementIndex,:));
+                    if(0 < size(component))
+                        for i = 1:size(component,2)
+                            component(i).model.base = tr_EndEffector*trotx(pi/2)*trotz(pi/2);
+                            animate(component(i).model, component(i).model.base);
+                        end
+                    end
+                    drawnow();
+                end 
+                    end  
+                    end
     end
 end
